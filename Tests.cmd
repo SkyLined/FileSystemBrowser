7803a64297c6f2e238cbf8813e2973afca33f8a8
SkyLined <github@skylined.nl>
2020-06-18 10:31:19 +0200

    Create Tests.cmd

---
+++ Tests.cmd
@@ -0,0 +1,32 @@
+@ECHO OFF
+SETLOCAL
+SET TEST_FOLDER_PATH=%TEMP%\FileSystemBrowser Test folder %RANDOM%
+SET TEST_ZIP_FILE_PATH=%TEMP%\zyp Test file %RANDOM%.zip
+
+MKDIR "%TEST_FOLDER_PATH%"
+
+ECHO   * Test version check...
+CALL "%~dp0FileSystemBrowser.cmd" --version
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test help...
+CALL "%~dp0FileSystemBrowser.cmd" --help
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test offline...
+CALL "%~dp0FileSystemBrowser.cmd" --offline="%TEST_FOLDER_PATH%"
+IF ERRORLEVEL 1 GOTO :ERROR
+ECHO   * Test offline with sharepoint hacks... 
+CALL "%~dp0FileSystemBrowser.cmd" --offline="%TEST_FOLDER_PATH%" --apply-sharepoint-hacks
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO "%TEST_FOLDER_PATH%" /S /Q
+
+ECHO + Done.
+EXIT /B 0
+
+:ERROR
+  ECHO     - Failed with error level %ERRORLEVEL%
+  IF EXIST "%TEST_FOLDER_PATH%" RD "%TEST_FOLDER_PATH%" /S /Q
+  ENDLOCAL
+  EXIT /B 1
